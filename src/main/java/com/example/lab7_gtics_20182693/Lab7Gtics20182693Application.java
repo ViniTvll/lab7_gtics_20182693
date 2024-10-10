3390ba55f17e62ecd82d1d1178eaaef0ddd3e63f
ViniTvll <vinitavella18@gmail.com>
2024-10-09 19:05:16 -0500

    .

---
+++ src/main/java/com/example/lab7_gtics_20182693/Lab7Gtics20182693Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics_20182693;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7Gtics20182693Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7Gtics20182693Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab7_gtics_20182693/Lab7Gtics20182693ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics_20182693;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7Gtics20182693ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
